11cb9d776ebb4fdaab0ee1a9f636b6db4816bce6
ks <ksainte19@gmail.com>
2024-03-16 18:14:21 +0100

    Now the deposit to Aave is made from funds in the contract balance

--- MonitorAaveV3.sol
+++ MonitorAaveV3.sol
@@ -10,46 +10,57 @@ import {IWrappedTokenGatewayV3} from "./IWrappedTokenGatewayV3.sol";
 
 
 contract MonitorAaveV3
-
 {
+       event Received(address, uint);
+    
+        receive() external payable {
+        emit Received(msg.sender, msg.value);
+        }   
 
-       uint256 public healthFactor;
+        address public AaveLendingAddress = 0x8dA9412AbB78db20d0B496573D9066C474eA21B8;
+        address public WrappedMaticTokenAddress = 0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a;
+        address public OnBehalfOf = 0x4415bd986532a2B8A1e702b18EF4A65075d42Db8;
+        address public AaveProxyLendingAddress = 0xcC6114B983E4Ed2737E9BD3961c9924e6216c704;
 
-function getHealthFactor() external view returns (uint256) {
-        (, , , , , uint256 healthFactor) = IPoolAaveV3(0xcC6114B983E4Ed2737E9BD3961c9924e6216c704)
-            .getUserAccountData(0x4415bd986532a2B8A1e702b18EF4A65075d42Db8);
+function getHealthFactor(address ProxyAddress, address OnWhichBehalf) external view returns (uint256) {
+        (, , , , , uint256 healthFactor) = IPoolAaveV3(ProxyAddress)
+            .getUserAccountData(OnWhichBehalf);
         
-        return healthFactor;
+            return healthFactor;
     }
 
-    function gettotalDebtBase() external view returns (uint256) {
+    function gettotalDebtBase(address ProxyAddress, address OnWhichBehalf) external view returns (uint256) {
 
-               (, uint256 totalDebtBase, , , , uint256 healthFactor) = IPoolAaveV3(
-            0xcC6114B983E4Ed2737E9BD3961c9924e6216c704
-        ).getUserAccountData(0x4415bd986532a2B8A1e702b18EF4A65075d42Db8);
+               (, uint256 totalDebtBase, , , ,) = IPoolAaveV3(
+            ProxyAddress).getUserAccountData(OnWhichBehalf);
+
         return totalDebtBase;
     }
 
-    function approve() public {
+    function approve(address token, address spender, uint256 amount) public {
             // IERC20(0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a).approve(0x8dA9412AbB78db20d0B496573D9066C474eA21B8, 500000000000000000);
             
-            IERC20 maticContract = IERC20(0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a);
+            IERC20 maticContract = IERC20(token);//0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a
 
-            maticContract.approve(0x8dA9412AbB78db20d0B496573D9066C474eA21B8, 500000000000000000);    
+            maticContract.approve(spender, amount);    
             
         }
 
+    
+ 
+        function fund() external payable {
+        
+        emit Received(msg.sender, msg.value);
+           
+        }
 
-        address public aaveContractAddress = 0x8dA9412AbB78db20d0B496573D9066C474eA21B8;
+    
 
     function deposit(uint256 amount) external payable {
 
 
-        IWrappedTokenGatewayV3 aaveContract = IWrappedTokenGatewayV3(0x8dA9412AbB78db20d0B496573D9066C474eA21B8);
-        // (bool success, ) = aaveContractAddress.call{value: amount}("");
-        // aaveContract.depositETH{value: msg.value}(0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a, 0x4415bd986532a2B8A1e702b18EF4A65075d42Db8, 0);
-        // require(success, "Transfer failed.");
-        aaveContract.depositETH{value: amount}(0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a, 0x4415bd986532a2B8A1e702b18EF4A65075d42Db8, 0);    
+        IWrappedTokenGatewayV3 aaveContract = IWrappedTokenGatewayV3(AaveLendingAddress);
+        aaveContract.depositETH{value: amount}(WrappedMaticTokenAddress, OnBehalfOf, 0);    
         }
 
     }
