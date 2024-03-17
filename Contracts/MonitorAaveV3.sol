1ed07b86c65f8ccc896f6050130677ba52479581
ks <ksainte19@gmail.com>
2024-03-17 09:01:00 +0100

    Last functions to MonitorAaveV3

--- Contracts/MonitorAaveV3.sol
+++ Contracts/MonitorAaveV3.sol
@@ -18,11 +18,15 @@ contract MonitorAaveV3
     address public AaveLendingAddress = 0x8dA9412AbB78db20d0B496573D9066C474eA21B8;
     address public WrappedMaticTokenAddress = 0xaD3C5a67275dE4b5554CdD1d961e957f408eF75a;
     address public AaveProxyLendingAddress = 0xcC6114B983E4Ed2737E9BD3961c9924e6216c704;
-
+    address public BorrowAndRepayTokenAddress = 0x52D800ca262522580CeBAD275395ca6e7598C014;
     address public aPolWmatic = 0xaCA5e6a7117F54B34B476aB95Bf3034c304e7a81;
+    uint256 public interestRateMode = 2;
+    uint256 public referralCode = 0;
 
     constructor() {
         IERC20(aPolWmatic).approve(AaveLendingAddress, type(uint256).max);
+        IERC20(BorrowAndRepayTokenAddress).approve(AaveProxyLendingAddress, type(uint256).max);
+
     }
 
 
@@ -56,19 +60,29 @@ contract MonitorAaveV3
 
     receive() external payable {
         IWrappedTokenGatewayV3 aaveContract = IWrappedTokenGatewayV3(AaveLendingAddress);
-        aaveContract.depositETH{value: msg.value}(WrappedMaticTokenAddress, address(this), 0);    
+        aaveContract.depositETH{value: msg.value}(WrappedMaticTokenAddress, address(this), referralCode);    
         emit Received(msg.sender, msg.value);
     }
     
 
     function deposit(uint256 amount) external payable {
         IWrappedTokenGatewayV3 aaveContract = IWrappedTokenGatewayV3(AaveLendingAddress);
-        aaveContract.depositETH{value: amount}(WrappedMaticTokenAddress, address(this), 0);    
+        aaveContract.depositETH{value: amount}(WrappedMaticTokenAddress, address(this), referralCode);    
     }
 
     function withdraw(uint256 amount) public {
         IWrappedTokenGatewayV3 aaveContract = IWrappedTokenGatewayV3(AaveLendingAddress);
         aaveContract.withdrawETH(aPolWmatic, amount, address(this));    
+    }
+
+    function borrow(uint256 amount) public {
+        IPoolAaveV3 aaveContract = IPoolAaveV3(AaveProxyLendingAddress);
+        aaveContract.borrow(BorrowAndRepayTokenAddress, amount, interestRateMode, referralCode, address(this));    
+    }
+
+    function repay(uint256 amount) public {
+        IPoolAaveV3 aaveContract = IPoolAaveV3(AaveProxyLendingAddress);
+        aaveContract.repay(BorrowAndRepayTokenAddress, amount, interestRateMode, address(this));    
     }
 
 }
